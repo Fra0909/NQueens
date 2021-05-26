8f82ee14b4f16b9120de691ec4536445b2ef4972
Francesco Romano <unconfigured@null.spigotmc.org>
2021-05-26 17:55:51 +0100

    Small Fixes and minor improvement to the GUI

--- GUI.py
+++ GUI.py
@@ -1,5 +1,4 @@
 from tkinter import *
-import random
 root = Tk()
 
 class GUI():
@@ -8,26 +7,29 @@ class GUI():
         self.size = size
         self.solutions = solutions
 
-    def createBoard(self, index = 0):
-        index = random.randint(0,92)
+    def get_new_solution(self, index, step):
+        if index == len(self.solutions)-1 and step:
+            self.createBoard(0)
+            return
+        if index == 0 and not step:
+            self.createBoard(len(self.solutions)-1)
+            return
+        if step:
+            print(index)
+            self.createBoard(index+1)
+            return
+        self.createBoard(index-1)
+
+    def createBoard(self, index = 0, last_colour = True):
         solutions = self.solutions[index]
-        if index == 92:
-            index = 0
-        elif index == -92:
-            index = 0
-        last_colour = True
         for i in range(self.size):
-            if last_colour == True:
-                last_colour = False
-            else:
-                last_colour = True
+            last_colour = not last_colour if self.size %2 == 0 else last_colour
             for j in range (self.size):
                 if solutions[i] == j:
-                    label = Label(root, text = '♛', font=("courier",self.size), width = 8, height = 4, fg='yellow')
+                    label = Label(root, text = '♛', font=("courier",8), width = 8, height = 4, fg='yellow',)
                 else:
-                    label = Label(root, text='', font=("courier", self.size), width=8, height=4)
-
-                if last_colour == True:
+                    label = Label(root, text='', font=("courier",8), width=8, height=4)
+                if last_colour:
                     label.config(bg='#000')
                     last_colour = False
                 else:
@@ -35,9 +37,13 @@ class GUI():
                     last_colour = True
 
                 label.grid(row = i,column = j)
-        prev = Button(root, text ='<', command = lambda: self.createBoard(index-1))
-        prev.grid(row = self.size + 1, column = int(self.size/2)-1)
-        next = Button(root, text='>', command = lambda: self.createBoard(index+1))
-        next.grid(row=self.size + 1, column=int(self.size/2))
+        prev = Button(root, text ='<', command = lambda: self.get_new_solution(index,False))
+        prev.grid(row = self.size, column = int(self.size)-3)
+        zero = Button(root, text='⌂', command=lambda: self.get_new_solution(len(self.solutions)-1, False))
+        zero.grid(row=self.size, column=int(self.size) - 2)
+        label2 = Label(root,text ='{}/{}'.format(index+1,len(self.solutions)))
+        label2.grid(row = self.size,column=0)
+        next = Button(root, text='>', command = lambda: self.get_new_solution(index,True))
+        next.grid(row=self.size, column=int(self.size)-1)
+        root.title("NQueens")
         root.mainloop()
-
